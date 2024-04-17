4fd8139db1a785f9494b95bd971ff8967eba1e69
Faizan Anwar <Faizana1298@gmail.com>
2024-04-17 19:35:41 +0530

    Adding root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :tasks
   devise_for :users
+
+  devise_scope :user do
+    root 'tasks#index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
