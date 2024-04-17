8e7e829b257c1deb0e86b0e86610ac6ba564cb89
Faizan Anwar <Faizana1298@gmail.com>
2024-04-17 19:59:54 +0530

    Adding authentication to the top level and error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+
+  def render_404
+    render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     root 'tasks#index'
   end
+
+  match "*path", to: "application#render_404", via: :all
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
